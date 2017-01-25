8288c3170673eda23858cab56b285501724655b3
anonunfeeling <anonunfeeling1@gmail.com>
2017-01-25 12:03:21 +0200

    add users group repository

---
+++ src/main/java/com/jdroidcoder/persistent/entity/GameEventMulti.java
@@ -0,0 +1,10 @@
+package com.jdroidcoder.persistent.entity;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by jdroidcoder on 25.01.17.
+ */
+@Entity
+public class GameEventMulti extends AbstractEntity<Long> {
+}
---
+++ src/main/java/com/jdroidcoder/persistent/repository/UseresGroupRepository.java
@@ -0,0 +1,10 @@
+package com.jdroidcoder.persistent.repository;
+
+import com.jdroidcoder.persistent.entity.UsersGroupEntity;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by jdroidcoder on 25.01.17.
+ */
+public interface UseresGroupRepository extends CrudRepository<UsersGroupEntity,Long> {
+}
